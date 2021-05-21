b600b2278985c76064f82c398e5850262590fb5a
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-05-21 11:19:13 +0100

    Implement Wifaq Prefs

--- src/main/java/uk/co/wifaqululama/adhan/WifaqDefaultCalculationParams.kt
+++ src/main/java/uk/co/wifaqululama/adhan/WifaqDefaultCalculationParams.kt
@@ -1,15 +1,17 @@
 package uk.co.wifaqululama.adhan
 
+import com.batoulapps.adhan.CalculationMethod
 import com.batoulapps.adhan.CalculationParameters
 import com.batoulapps.adhan.Madhab
 
 object WifaqDefaultCalculationParams {
 
     fun getCalculationParams(): CalculationParameters{
-        val parameters = CalculationParameters(18.0,18.0)
+        val parameters = CalculationMethod.KARACHI.parameters
+        parameters.ishaAngle = 15.0
         parameters.madhab = Madhab.HANAFI
         parameters.adjustments.maghrib = 5
-        parameters.adjustments.dhuhr = 5
+        parameters.adjustments.dhuhr = 4
         return parameters
     }
 }
