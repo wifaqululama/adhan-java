d6ced5451bcac643e8eeef6082d7510c121835e0
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2021-06-03 13:10:42 +0100

    Update Aqrabul Ayyam Values

--- src/main/java/uk/co/wifaqululama/adhan/AqrabulAyyam.kt
+++ src/main/java/uk/co/wifaqululama/adhan/AqrabulAyyam.kt
@@ -4,10 +4,8 @@ import com.batoulapps.adhan.Coordinates
 import com.batoulapps.adhan.data.DateComponents
 import com.batoulapps.adhan.data.TimeComponents
 import com.batoulapps.adhan.internal.SolarTime
-import jdk.vm.ci.meta.Local
 import java.time.LocalDate
 import java.time.LocalTime
-import java.util.*
 
 /**
  * Helper Class to calculate Aqrabul Ayyam
@@ -38,39 +36,39 @@ class AqrabulAyyam(val coordinates: Coordinates) {
         val latitude = coordinates.latitude
         if(latitude > 48.89){
             fajrAQ = when{
-                latitude > 49.89 -> 1.68
-                latitude > 50 ->  1.7
-                latitude > 50.25 -> 1.68
-                latitude > 50.5 -> 1.45
-                latitude > 50.75 -> 1.38
-                latitude > 51 -> 1.48
-                latitude > 51.25 -> 1.47
-                latitude > 51.5 -> 1.43
-                latitude > 51.75 -> 1.45
-                latitude > 52 -> 1.52
-                latitude > 52.25 -> 1.47
-                latitude > 52.5 -> 1.37
-                latitude > 52.75 -> 1.52
-                latitude > 53 -> 1.47
-                latitude > 53.25 -> 1.62
-                latitude > 53.5 -> 1.57
-                latitude > 53.75 -> 1.57
-                latitude > 54 -> 1.6
-                latitude > 54.5 -> 1.63
-                latitude > 55 -> 1.82
-                latitude > 55.5 -> 1.95
-                latitude > 56 -> 1.92
-                latitude > 56.5 -> 1.9
-                latitude > 57 -> 2.03
-                latitude > 57.5 -> 1.75
+                latitude > 49.89 && latitude <= 50-> 1.68
+                latitude > 50 && latitude <= 50.25 ->  1.7
+                latitude > 50.25 && latitude <= 50.5 -> 1.68
+                latitude > 50.5 && latitude <= 50.75 -> 1.45
+                latitude > 50.75 && latitude <= 51 -> 1.38
+                latitude > 51 && latitude <= 51.25-> 1.48
+                latitude > 51.25 && latitude <= 51.5 -> 1.47
+                latitude > 51.5 && latitude <= 51.75 -> 1.43
+                latitude > 51.75 && latitude <= 52 -> 1.45
+                latitude > 52 && latitude <= 52.25 -> 1.52
+                latitude > 52.25 && latitude <= 52.5 -> 1.47
+                latitude > 52.5 && latitude <= 52.75 -> 1.37
+                latitude > 52.75 && latitude <= 53 -> 1.52
+                latitude > 53 && latitude <= 53.25 -> 1.47
+                latitude > 53.25 && latitude <= 53.5 -> 1.62
+                latitude > 53.5 && latitude <= 53.75-> 1.57
+                latitude > 53.75 && latitude <= 54 -> 1.6
+                latitude > 54 && latitude <= 54.5 -> 1.55
+                latitude > 54.5 && latitude <= 55 -> 1.63
+                latitude > 55 && latitude <= 55.5 -> 1.82
+                latitude > 55.5 && latitude <= 56 -> 1.95
+                latitude > 56 && latitude <= 56.5 -> 1.92
+                latitude > 56.5 && latitude <= 57 -> 1.9
+                latitude > 57 && latitude <= 57.5 -> 2.03
+                latitude > 57.5 && latitude <= 59 -> 1.83
                 latitude > 59 -> 1.75
                 else -> {
-                  1.78 // TODO return last second-to-last date and save that instead here.
+                  1.78 // Unreachable Code
                 }
             }
         }
-        val t = (100 * fajrAQ)
-        val fajrOffsetMins = t / 100
-        return time.plusMinutes(fajrOffsetMins.toLong())
+        println("Latitude is $latitude and fajrAQ is $fajrAQ")
+        val t = (60 * fajrAQ)
+        return time.plusMinutes(t.toLong())
     }
 }
